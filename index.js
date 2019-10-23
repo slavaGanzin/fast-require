b0af0bce4c6a2bf04362f99c6faefb39af0f46d3
Slava Ganzin <slava.ganzin@gmail.com>
2019-10-23 19:22:46 +0300

    eslint

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const path = require('path')
 
-module.exports = (options) => {
+module.exports = options => {
   const {only, without, search, globally, as, toRoot} = Object.assign({
-    only: null, without: [], search: [process.cwd()], globally: false, as: {}, toRoot: []
+    only:     null,
+    without:  [],
+    search:   [process.cwd()],
+    globally: false,
+    as:       {},
+    toRoot:   [],
   }, options)
 
   const packages = globally ? global : {}
@@ -10,24 +15,24 @@ module.exports = (options) => {
   for (const dir of search) {
     const packageJSON = require(path.resolve(dir, 'package.json'))
 
-    if (only) for (const i in only) {
-      only[only[i]] = only[i]
-      delete only[i]
+    if (only) {
+      for (const i in only) {
+        only[only[i]] = only[i]
+        delete only[i]
+      }
     }
 
     for (const package in only || Object.assign(packageJSON.dependencies || {}, packageJSON.devDependencies || {})) {
       if (!only && without.indexOf(package) > -1) continue
 
-      const name = as[package] || package.replace(/(-)([^-])/g, (all, dash, symbol) => symbol.toUpperCase())
+      const name = as[package] || package.replace(/(\.|-)([^-])/g, (all, dash, symbol) => symbol.toUpperCase())
       const module = require(require.resolve(package, {paths: [dir]}))
 
       if (toRoot.indexOf(package) > -1) {
-        for (const f in module) {
+        for (const f in module)
           packages[f] = module[f]
-        }
-      } else {
+      } else
         packages[name] = module
-      }
     }
   }
 
